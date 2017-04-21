d15ed8ab54e260dbb2255b3c740f3d1f8162f06e
Dennis <perk430@gmail.com>
2017-04-21 14:25:13 -0400

    Initial commit of Bonus 18

---
+++ Bonus18.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonus18
+{
+    class Car
+    {
+        private string make;
+        private string model;
+        private int year;
+        private double price;
+
+        //No arguments constructor setting data members to default values
+        public Car()
+        {
+            make = "";
+            model = "";
+            year = 2017;
+            price = 72000;
+
+        }
+        //Constructor with four arguments matching the above order
+        public Car(string makeInput, string modelInput, 
+            int yearInput, double PriceInput)
+        {
+            make = makeInput;
+            model = modelInput;
+            year = yearInput;
+            price = PriceInput;
+        }
+
+        public string CarMake
+
+        {
+            set
+            {
+                make = value;
+            }
+
+            get
+            {
+                return make;
+            }
+        }
+
+        public string CarModel
+        {
+            set
+            {
+                model = value;
+            }
+            get
+            {
+                return model;
+            }
+        }
+
+        public int CarYear
+        {
+            set
+            {
+                year = value;
+            }
+
+            get
+            {
+                return year;
+            }
+        }
+
+        public double CarPrice
+
+        {
+            set
+            {
+                price = value;
+            }
+
+            get
+            {
+                return price;
+            }
+        }
+    }
+}
+
+
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonus18
+{
+    class CarApp
+    {
+        static void Main(string[] args)
+        {
+        
+            Console.WriteLine("Welcome to the Grand Circus Motors admin console! \n\n");
+
+            //Console.WriteLine("How many cars are you entering?  ");
+            Console.WriteLine("Enter Car1 Make \n");
+            string make=  Console.ReadLine();
+
+            Console.WriteLine("Enter Car1 Model \n");
+            string model = Console.ReadLine();
+
+            Console.WriteLine("Enter Car1 year \n");
+            int year = int.Parse(Console.ReadLine());//Parse to read the "integer" value
+
+            Console.WriteLine("Enter Car1 price");
+            double price = double.Parse(Console.ReadLine());//Parse to read the "double" value
+
+
+            Car Car1 = new Car(make, model, year, price);
+
+            Console.WriteLine(Car1.CarMake + "\t" + Car1.CarModel + "\t" + Car1.CarYear + "\t" + Car1.CarPrice);
+                 
+        }
+    }
+}
